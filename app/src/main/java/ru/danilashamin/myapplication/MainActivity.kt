d3848fe3c459908f6965d076205104cc9b3d0ca0
danilashamin <danilashamin@gmail.com>
2024-10-21 20:18:30 +0300

    Initial commit

---
+++ app/src/main/java/ru/danilashamin/myapplication/MainActivity.kt
@@ -0,0 +1,17 @@
+package ru.danilashamin.myapplication
+
+import android.os.Bundle
+import android.view.Menu
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+}
